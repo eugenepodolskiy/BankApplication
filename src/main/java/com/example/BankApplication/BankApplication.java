32bfde6855f68d374caa8a6dbba04f4372daa84f
eugenepodolskiy <eugenepodolskiy@gmail.com>
2022-07-12 12:58:45 +0300

    initial commit

---
+++ src/main/java/com/example/BankApplication/BankApplication.java
@@ -0,0 +1,24 @@
+package com.example.BankApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.addDialect(sec); // Enable use of "sec"
+		return templateEngine;
+	}
+}
---
+++ src/main/java/com/example/BankApplication/appuser/AppUserRepo.java
@@ -0,0 +1,4 @@
+package com.example.BankApplication.appuser;
+
+public interface AppUserRepo {
+}
